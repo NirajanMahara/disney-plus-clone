de0959044709af5fadc33ec3b4fbc7c7975d2223
Nirajan Mahara <mnirajan9@gmail.com>
2021-05-29 16:29:35 +0545

    ADD: Background 🖼 Image of Login

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,7 +4,7 @@ const Login = (props) => {
     return (
         <Container>
             <Content>
-                Content
+                <BgImage />
             </Content>
         </Container>
     )
@@ -31,6 +31,19 @@ const Content = styled.div`
     flex-direction: column;
     padding: 80px 40px;
     height: 100%;
+`
+
+const BgImage = styled.div`
+    height: 100%;
+    background-position: top;
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-image: url("/images/login-background.jpg");
+    position: absolute;
+    top: 0;
+    right: 0;
+    left: 0;
+    z-index: -1;
 `
 
 export default Login;
