430f80d81f0e9acb30876772bb99353c2a43b3b2
Nirajan Mahara <mnirajan9@gmail.com>
2021-05-29 18:31:40 +0545

    ADD: Description

--- src/components/Login.js
+++ src/components/Login.js
@@ -7,6 +7,11 @@ const Login = (props) => {
                 <CTA>
                     <CTALogoOne src="/images/cta-logo-one.svg" alt="" />
                     <SignUp>Get all there</SignUp>
+                    <Description>
+                        Get Premier Access to Raya and the Last Dragon for an additional fee
+                        with a Disney+ subscription. As of 03/26/21, the price of Disney+
+                        and The Disney Bundle will increase by $1.
+                    </Description>
                 </CTA>
 
                 <BgImage />
@@ -83,6 +88,14 @@ const SignUp = styled.a`
         background-color: #0483ee;
         cursor: pointer;
     }
+`
+
+const Description = styled.p`
+    font-size: 11px;
+    margin: 0 0 24px;
+    line-height: 1.5;
+    letter-spacing: 1.5px;
+    color: hsla(0, 0%, 95.3%, 1);
 `
 
 export default Login;
